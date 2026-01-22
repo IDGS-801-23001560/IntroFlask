c47156e9a6282a56ca0df1685e50e9c3f5e93748
Jael Vargas <175512004+Jael-Vargas@users.noreply.github.com>
2026-01-21 18:48:19 -0600

    Rutas

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+@app.route("/hola")
+def hola():
+    return "¡Hola, Mundo!"
+
+@app.route("/user/<string:user>")
+def user(user):
+    return f"Hello, {user}!"
+
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f"<h1>El numero es: {n}</h1>"
+
+@app.route("/user/<int:id>/<string:username>")
+def username(us,username):
+    return f"<h1>¡Hola, {username}! Tu ID es: {id}"
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1,n2):
+    return f"<h1>La suma es: {n1+n2}</h1>"
+
+@app.route("/default")
+@app.route("/default/<string:parm>")
+def func(param="juan"):
+    return f"<h1>¡Hola, {param}!</h1>"
+
+@app.route("operas")
+def operas():
+    return '''
+        <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        </br>
+        <label for="name">apaterno:</label>
+        <input type="text" id="name" name="name" required>
+    </form>
+'''
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
